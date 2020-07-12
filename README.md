2f8ad18c2e91f549a9d0a1dc30925803bb46bb02
Malcome Baggett <malcomebaggett@gmail.com>
2020-07-11 23:30:24 -0700

    added README

---
+++ README.md
@@ -0,0 +1,12 @@
+# CovidBase
+
+CovidBase is a website which allows users to check Covid-19 statistics by specific States within the USA.
+
+## Getting Started
+
+Getting started is simple download the repo and launch the index.html within the browser
+
+## Built With
+
+- [Vue.js](https://vuejs.org/) - Front End Framework
+- [Materialize](https://materializecss.com/) - Material Design Components
--- client/main.js
+++ client/main.js
@@ -1,9 +1,22 @@
 const app = new Vue({
-    el: '#container',
-    data: {
-        oklahomaData: null,
-    },
-    mounted() {
-        axios.get('https://covidtracking.com/api/v1/states/ok/daily.json').then(res => this.oklahomaData = res)
-    }
-})
+  el: "#container",
+  data: {
+    oklahomaData: [null],
+  },
+  mounted() {
+    axios
+      .get("https://covidtracking.com/api/v1/states/ok/daily.json")
+      .then((res) => {
+        this.oklahomaData = res.data;
+      })
+      .catch((err) => {
+        window.location.href = "/";
+        console.log(err);
+      });
+  },
+  // computed() {
+  //     getDate(date) {
+
+  //     }
+  // }
+});
